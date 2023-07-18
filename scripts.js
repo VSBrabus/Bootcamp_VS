6f6e2981f2ea27f3611922a817ff63041a4193bf
VSBrabus <87549817+VSBrabus@users.noreply.github.com>
2023-07-19 02:48:40 +0300

    Refactored peoject day 2 homework

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    const outputElement = document.getElementById('output');
+    outputElement.textContent = 'Button clicked!';
+  });

9ab2116035cb4c4b4ee9f1a4167fe35562c3b8d2
VSBrabus <87549817+VSBrabus@users.noreply.github.com>
2023-07-20 20:08:33 +0300

    Homework day 4

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
-document.getElementById('myButton').addEventListener('click', function() {
-    const outputElement = document.getElementById('output');
-    outputElement.textContent = 'Button clicked!';
-  });
+const clickCountElement = document.getElementById('clickCount');
+const shoppingCartImageElement=document.getElementById('shopping-cart-image')
+// Initialize click count
+let clickCount = 0;
+
+// Function to update the click count and display it
+function updateClickCount() {
+    clickCount++;
+    clickCountElement.textContent = clickCount;
+    shoppingCartImageElement.src = "/images/Shopping_Cart_fill.svg"
+  }
